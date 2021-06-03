9bd0080bcef2b0d52ede367d93f19561896c64c7
muhyasin89 <muhyasin89@gmail.com>
2021-06-03 16:39:31 +0700

    [master]common character count

--- codesignals/arcade/commonCharacterCount.py
+++ codesignals/arcade/commonCharacterCount.py
@@ -1,25 +1,42 @@
 
-s1 = "aabcc"
-s2 = "adcaa"
+# s1 = "aabcc"
+# s2 = "adcaa"
+
+s1 = "abca"
+s2 = "xyzbac"
+
+# s1="zzzz"
+# s2="zzzzzzz"
+
+# def commonCharacterCount(s1, s2):
+#     # find from s1 as based the duplicate character
+#     dict_count = {}
+
+#     for char in s1:
+#         counts=s1.count(char)
+#         if counts > 1 or char in s2:
+#             dict_count[char] = counts
+            
+#     for char in s2:
+#         counts = s2.count(char)
+#         if char in dict_count.keys() and dict_count[char] > counts:
+#             dict_count[char] = counts
+                
+#     return sum(dict_count.values())
+
+#the result is
 
 def commonCharacterCount(s1, s2):
-    list_count = []
+    #remove duplicate s1
+    s1_set = set(s1)
+    result = 0
 
-    #split all chacter into list
-    list_s1 = list(s1)
-    list_s2 = list(s2)
-
-    #compare s1 to s2 O(n)
-    for item_s1 in list_s1:
-        #set variable to count the duplicate
-        count = 0
-        for item_s2 in list_s2:
-            if item_s1 == item_s2:
-                count+=1
-        list_count.append(count)
-
-    return max(list_count)
-
-
+    #count s1 set and count from both s1 and s2
+    for letter in s1_set:
+        s1_count = s1.count(letter)
+        s2_count = s2.count(letter)
+        #result from min of char count
+        result += min([s1_count, s2_count])
+    return result
 
 print(commonCharacterCount(s1, s2))
