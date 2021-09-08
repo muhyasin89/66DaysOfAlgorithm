29042c00b9d5a9ce83dfe449d60376c1c02bf2ec
muhammad yasin <muhyasin89@gmail.com>
2021-09-08 16:45:48 +0700

    [test]add question guidelines

---
+++ PlayGround/Resource_go/Youtube/List.go
@@ -0,0 +1,25 @@
+
+
+// make string and int
+
+// turn string into list
+
+// turn list into string
+
+//swap list
+
+// make another list
+
+// merge 2 list with same type
+
+// check if 'k' inside list
+
+// check index space
+
+// remove space in list
+
+// make hash map
+
+// check if n in keys
+
+// check if n in values
--- PlayGround/Resource_javascript/Basic/Youtube/List.js
+++ PlayGround/Resource_javascript/Basic/Youtube/List.js
@@ -1,71 +1,27 @@
-let first_string = "Hello World"
-let first_int = 11223344
+// make string "Hello World" and int "11223344"
 
-let list_str = first_string.split("");
-// Array.from(first_int);
-// Object.assign([], first_int);
+// turn string into list
 
-let list_int = Array.from(first_int.toString());
+// turn int to string and make list
 
-const toNum =  (list_int) = list_int.map((i) => Number(i));
+// turn list character into list int
 
-const set1 = new Set(list_int);
+// turn list into string
 
-// let arraySet = Array.from(set1);
-let arraySet = [...set1];
+//swap list 
 
-list_a = [2, 4, 3, 5, 1]
+// make another list
 
+// merge 2 list with same type
 
-console.log(list_str);
-list_str.splice(5, 1);
-console.log(list_str);
+// check if 'k' inside list
 
-console.log(list_int);
-// console.log(toNum(list_int));
+// check index space
 
-console.log(set1);
+// remove space in list
 
-console.log(arraySet)
+// make hash map
 
-mid = Math.floor(arraySet.length/2);
+// check if n in keys
 
-left = arraySet.slice(0, mid);
-right = arraySet.slice(mid, arraySet.length);
-
-console.log(left);
-console.log(right);
-
-
-[left[0], left[1]] = [left[1], left[0]];
-
-console.log(left);
-
-const map1 = new Map();
-
-map1.set('a', 1);
-map1.set('b', 2);
-map1.set('c', 3);
-
-console.log(map1.get('a'));
-
-console.log(map1.has('b'));
-
-
-const findInMap = (map1, val) => {
-    for(let [k, v] of map1){
-        if (v == val){
-            return true
-        }
-    }
-    return false;
-}
-console.log(findInMap(map1, 4));
-
-const itterateHashMap = (map1) =>{
-    for(const [key, value] of map1.entries()){
-        console.log(key +" =" + value);
-    }
-}
-
-itterateHashMap(map1)
+// check if n in values
