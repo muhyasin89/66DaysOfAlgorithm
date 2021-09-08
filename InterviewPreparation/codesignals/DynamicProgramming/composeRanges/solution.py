5a7ee6903fd385f7976842ee15acb0ac9870065a
muhammad yasin <muhyasin89@gmail.com>
2021-09-09 04:12:34 +0700

    [master]restructure code

--- InterviewPreparation/codesignals/DynamicProgramming/composeRanges/solution.py
+++
@@ -1,9 +0,0 @@
-def composeRanges(nums):
-
-    ranges = []
-    while nums:
-        start = end = nums.pop(0)
-        while nums and nums[0] - end == 1:
-            end = nums.pop(0)
-        ranges.append(str(start) + ("", "->" + str(end))[start != end])
-    return ranges
