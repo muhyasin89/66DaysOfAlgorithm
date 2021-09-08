42ead6e15c847d5f2f4d21187e8eef44759bd9c6
muhammad yasin <muhyasin89@gmail.com>
2021-09-08 16:48:38 +0700

    [master]add guideline for Youtube

---
+++ PlayGround/Resource_php/Basic/Youtube/List.php
@@ -0,0 +1,28 @@
+<?php
+// make string "Hello World" and int "11223344"
+
+// turn string into list
+
+// turn int to string and make list
+
+// turn list character into list int
+
+// turn list into string
+
+// swap list
+
+// make another list
+
+// merge 2 list with same type
+
+// check if 'k' inside list
+
+// check index space
+
+// remove space in list
+
+// make hash map
+
+// check if n in keys
+
+// check if n in values
