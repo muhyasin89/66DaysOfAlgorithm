0f2159f3f60973f68b7fcf38175d680d4369b2bc
muhammad yasin <muhyasin89@gmail.com>
2021-09-16 00:35:26 +0700

    [test]add valid bracket and word Following Pattern

--- Practice/Array/wordFollowingPattern/solution.js
+++ Practice/Array/wordFollowingPattern/solution.js
@@ -1,6 +1,32 @@
 string1 = "abba"
 str1 = "dog cat cat dog"
-# result True
+//result True
 
 string2 = "abba"
 str2 = "dog cat cat fish"
+//return False
+
+
+const areFollowingPatterns = (s, str) =>{
+    m = new Map();
+    list_s = s.split("");
+    list_str = str.split(" ");
+
+    if(list_s.length != list_str.length){
+        return false;
+    }
+
+    for(let i=0;i< list_s.length; i++){
+        if(m.has(list_s[i])){
+            if(m.get(list_s[i]) != list_str[i]){
+                return false;
+            }
+        }else{
+            m[list_s[i]] = list_str[i]
+        }
+    }
+    return true;
+}
+
+console.log(areFollowingPatterns(str1, string1));
+console.log(areFollowingPatterns(str2, string2));
--- Practice/Array/wordFollowingPattern/solution.rb
+++ Practice/Array/wordFollowingPattern/solution.rb
@@ -6,6 +6,28 @@ string2 = "abba"
 str2 = "dog cat cat fish"
 
 
-def checkPattern()
-    
+def areFollowingPatterns(s, str)
+    dict_map = {}
+    list_s = s.split("")
+    list_str = str.split(" ")
+
+    if list_s.length != list_str.length
+        return false
+    end
+
+    for i in 0..list_s.length
+        if dict_map.key?(list_s[i])
+            if dict_map[list_s[i]] != list_str[i]
+                return false
+            end
+        else
+            dict_map[list_s[i]] = list_str[i]
+        end
+    end
+
+    return true
 end
+
+
+puts areFollowingPatterns(string1, str1)
+puts areFollowingPatterns(string2, str2)
