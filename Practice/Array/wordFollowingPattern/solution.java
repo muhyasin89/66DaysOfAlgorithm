c11b9b64d423b038c4ce5a5ec9c9b66e9b85d419
muhammad yasin <muhyasin89@gmail.com>
2021-09-15 18:38:57 +0700

    [test]add solution word pattern

--- Practice/Array/wordFollowingPattern/solution.java
+++ Practice/Array/wordFollowingPattern/solution.java
@@ -1,7 +1,44 @@
 package Practice.Array.wordFollowingPattern;
 
-public class solution {
-    public static void main(String[] args) {
+import java.util.*;
 
+public class solution {
+    Boolean checkPattern(String s, String str) {
+        Map<Character, String> map_words = new HashMap<>();
+
+        String[] spl = new String[] {};
+        spl = str.split(" ");
+        if (s.length() != spl.length) {
+            return false;
+        }
+        byte[] strAsByteArray = s.getBytes();
+
+        for (int i = 0; i < strAsByteArray.length; i++) {
+            if (map_words.containsKey((char) strAsByteArray[i])) {
+                if ((String) map_words.get((char) strAsByteArray[i]).trim() != (String) spl[i].trim()) {
+                    System.out.println(map_words.get((char) strAsByteArray[i]) == spl[i]);
+                    return false;
+                }
+            } else {
+                map_words.put((char) strAsByteArray[i], spl[i]);
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String str1 = "abba";
+        String string1 = "dog cat cat dog";
+        // result True
+
+        String str2 = "abba";
+        String string2 = "dog cat cat fish";
+        // return False
+
+        solution obj = new solution();
+
+        System.out.println(obj.checkPattern(str1, string1));
+        System.out.println(obj.checkPattern(str2, string2));
     }
 }
