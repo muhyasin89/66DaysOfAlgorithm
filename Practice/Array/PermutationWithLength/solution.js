df23b1c7a038bd00ef0fa20b99f57238ba87878a
muhammad yasin <muhyasin89@gmail.com>
2021-09-20 09:01:13 +0700

    [test]add Permutation with length

--- Practice/Array/PermutationWithLength/solution.js
+++ Practice/Array/PermutationWithLength/solution.js
@@ -3,32 +3,30 @@ size = arr.length
 L=2
 
 
-const convertToLenTHBase = (n, arr, size, L) =>{
-    a = []
+const convertToLenTHBase = (result,n, arr, size, L) =>{
     for(let i=0; i< L;i++){
         result.push(arr[n % size]);
         n = Math.round(n/size);
     }
 
-    if(a.length > 0){
-        return a;
-    } else{
-        return null;
-    }
+    return result
 }
 
 const getAllPermutations = (arr, size, L) =>{
     result = []
-    temp = []
+    new_result = []
     for(let i=0; i< Math.pow(size, L); i++){
-        a = convertToLenTHBase(i, arr, size, L)
-        if(a){
-            temp.push(a)
-        }
-        // console.log(convertToLenTHBase(i, arr, size, L));
+        convertToLenTHBase(result,i, arr, size, L)
     }
 
-    return result
+    var i,j, temporary, L;
+    for (i = 0,j = result.length; i < j; i += L) {
+        temporary = result.slice(i, i + L);
+        // do whatever
+        new_result.push.apply(new_result, [temporary])
+    }
+
+    return new_result
 }
 
 console.log(getAllPermutations(arr, size, L));
