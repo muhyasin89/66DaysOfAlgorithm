3f5610d8ce5be120210728c0be6f666be164ec46
muhyasin89 <muhyasin89@gmail.com>
2022-03-20 07:57:46 +0700

    [test]add monoliticProgram

--- Basic/C++/monoliticProgram.cpp
+++ Basic/C++/monoliticProgram.cpp
@@ -4,5 +4,19 @@
 using namespace std;
 
 int main(){
+    int length = 0, breath=0;
+
+    printf("Enter Length and Breath\n");
+    std::cout << "length: ";
+    std::cin  >> length;
+
+    std::cout << endl << "Breath:" ;
+    std::cin >> breath;
+
+    int area =  length * breath;
+    int peri = 2* (length+breath);
+
+    printf("Area %d \n perimiter %d \n", area, peri);
+
     return 0;
 }
