a1ae1b1b09f5b823452272228fc08545730d8750
muhyasin89 <muhyasin89@gmail.com>
2022-04-05 10:59:04 +0700

    [test]add recursion

---
+++ Basic/C++/recursion/4_indirect_recursion.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void funB(int n);
+
+void funA(int n){
+    if(n> 0){
+        printf("A: %d ", n);
+        funB(n-1);
+    }
+}
+
+void funB(int n){
+    if(n>1){
+        printf("B: %d ",n);
+        funA(n/2);
+    }
+}
+
+int main(){
+    funA(20);
+    return 0;
+}
