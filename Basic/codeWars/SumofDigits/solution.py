169d4dd02fdb0d74d48a700d0c43f53531bf6103
muhyasin89 <muhyasin89@gmail.com>
2022-04-14 14:12:37 +0700

    [master]add codewars daily

---
+++ Basic/codeWars/SumofDigits/solution.py
@@ -0,0 +1,10 @@
+def digital_root(n):
+    if int(n) < 10:
+       return n
+    else:
+        res = [int(x) for x in str(n)]
+        print(res)
+        result = 0
+        for i in res:
+            result += i
+        return digital_root(result)
