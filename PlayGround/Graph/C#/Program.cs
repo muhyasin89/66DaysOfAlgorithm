fac472fd67fe408e778d39862dc66b6bacd27883
muhyasin89 <muhyasin89@gmail.com>
2022-04-15 14:49:08 +0700

    [test]graph

--- PlayGround/Graph/C#/Program.cs
+++ PlayGround/Graph/C#/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// C# code to demonstrate Graph representation
+// using LinkedList in C#
+using System;
+using System.Collections.Generic;
+
+class Graph {
+	// A utility function to add an edge in an
+	// undirected graph
+	static void addEdge(LinkedList<int>[] adj, int u, int v)
+	{
+		adj[u].AddLast(v);
+		adj[v].AddLast(u);
+	}
+
+	// A utility function to print the adjacency list
+	// representation of graph
+	static void printGraph(LinkedList<int>[] adj)
+	{
+		for (int i = 0; i < adj.Length; i++) {
+			Console.WriteLine("\nAdjacency list of vertex "
+							+ i);
+			Console.Write("head");
+
+			foreach(var item in adj[i])
+			{
+				Console.Write(" -> " + item);
+			}
+			Console.WriteLine();
+		}
+	}
+
+	// Driver Code
+	public static void Main(String[] args)
+	{
+		// Creating a graph with 5 vertices
+		int V = 5;
+		LinkedList<int>[] adj = new LinkedList<int>[ V ];
+
+		for (int i = 0; i < V; i++)
+			adj[i] = new LinkedList<int>();
+
+		// Adding edges one by one
+		addEdge(adj, 0, 1);
+		addEdge(adj, 0, 4);
+		addEdge(adj, 1, 2);
+		addEdge(adj, 1, 3);
+		addEdge(adj, 1, 4);
+		addEdge(adj, 2, 3);
+		addEdge(adj, 3, 4);
+
+		printGraph(adj);
+
+		Console.ReadKey();
+	}
+}
+
+// This code is contributed by techno2mahi
---
+++ PlayGround/Graph/Graph.js
@@ -0,0 +1,46 @@
+// Javascript code to demonstrate Graph representation
+// using ArrayList in Java
+ 
+// A utility function to add an edge in an
+    // undirected graph
+    function addEdge(adj,u,v)
+    {
+        adj[u].push(v);
+            adj[v].push(u);
+    }
+     
+    // A utility function to print the adjacency list
+        // representation of graph
+    function printGraph(adj)
+    {
+        for (let i = 0; i < adj.length; i++) {
+                console.log("Adjacency list of vertex " + i+"");
+                console.log("head");
+                for (let j = 0; j < adj[i].length; j++) {
+                    console.log(" -> "+adj[i][j]);
+                }
+                console.log("");
+            }
+    }
+     
+    // Driver Code
+    // Creating a graph with 5 vertices
+            let V = 5;
+            let adj= [];
+              
+            for (let i = 0; i < V; i++)
+                adj.push([]);
+      
+            // Adding edges one by one
+            addEdge(adj, 0, 1);
+            addEdge(adj, 0, 4);
+            addEdge(adj, 1, 2);
+            addEdge(adj, 1, 3);
+            addEdge(adj, 1, 4);
+            addEdge(adj, 2, 3);
+            addEdge(adj, 3, 4);
+              
+            printGraph(adj);
+     
+     
+    // This code is contributed by avanitrachhadiya2155
