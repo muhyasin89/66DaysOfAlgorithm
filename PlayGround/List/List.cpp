3399eeeeadbc28c6e5fd877a1939b78475461ba2
muhyasin89 <muhyasin89@gmail.com>
2022-04-19 13:03:40 +0700

    [master]add c#

---
+++ PlayGround/List/List.cpp
@@ -0,0 +1,41 @@
+int main(){
+    // make string "Hello World" and string "1122334455"
+
+    // turn string into list
+
+    // turn list string into list int
+
+    // turn list char into string
+
+    // remove duplicate
+
+    // check if 'space' inside list
+   
+    // check index space
+
+    //change List<str> into char[]
+
+    // remove space in list
+
+    // make another list
+ 
+    // merge 2 list with same type
+
+    // swap list
+  
+    //change array to vector
+   
+    // cut list into 2 left and right
+  
+    // make hash map
+
+    // check if n in keys
+
+    // get hash_map get value of k
+
+    // check if n in values
+   
+    // itterate hashmap
+    
+    return 0;
+}
